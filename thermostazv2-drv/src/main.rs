9568ce28ca436639765c77bab12ffea8125f9fa5
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-12-04 19:58:31 +0100

    parse log level from args

--- thermostazv2-drv/src/main.rs
+++ thermostazv2-drv/src/main.rs
@@ -3,12 +3,14 @@ use async_channel::unbounded;
 use clap::Parser;
 use futures::stream::StreamExt;
 use rumqttc::{AsyncClient, Event, LastWill, MqttOptions, Packet, QoS};
+use std::str::FromStr;
 use std::sync::{Arc, RwLock};
 use std::time::Duration;
 use thermostazv2_lib::{Cmd, Relay, SensorErr, SensorResult};
 use tokio::task;
 use tokio_serial::SerialPortBuilderExt;
 use tokio_util::codec::Decoder;
+use tracing::Level;
 
 mod err;
 mod sercon;
@@ -22,43 +24,48 @@ use crate::thermostazv::Thermostazv;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(env = "UART_PORT", default_value = "/dev/thermostazv2")]
+    #[arg(long, env = "UART_PORT", default_value = "/dev/thermostazv2")]
     uart_port: String,
 
-    //#[arg(env = "UART_BAUD", default_value = 2_000_000)]
-    //uart_baud: u32,
-    #[arg(env = "MQTT_HOST", default_value = "totoro")]
+    #[arg(long, env = "UART_BAUD", default_value_t = 2_000_000)]
+    uart_baud: u32,
+
+    #[arg(long, env = "MQTT_HOST", default_value = "totoro")]
     mqtt_host: String,
 
-    //#[arg(env = "MQTT_PORT", default_value = 1883)]
-    //mqtt_Port: i32,
-    #[arg(env = "MQTT_USER", default_value = "nim")]
+    #[arg(long, env = "MQTT_PORT", default_value_t = 1883)]
+    mqtt_port: u16,
+
+    #[arg(long, env = "MQTT_USER", default_value = "nim")]
     mqtt_user: String,
 
-    #[arg(env = "MQTT_PASS")]
+    #[arg(long, env = "MQTT_PASS")]
     mqtt_pass: String,
 
-    #[arg(env = "INFL_BUCK", default_value = "azviot")]
+    #[arg(long, env = "INFL_BUCK", default_value = "azviot")]
     infl_buck: String,
 
-    #[arg(env = "INFL_ORG", default_value = "azviot")]
+    #[arg(long, env = "INFL_ORG", default_value = "azviot")]
     infl_org: String,
 
-    #[arg(env = "INFL_URL", default_value = "http://localhost:8086")]
+    #[arg(long, env = "INFL_URL", default_value = "http://localhost:8086")]
     infl_url: String,
 
-    #[arg(env = "INFL_TOKEN")]
+    #[arg(long, env = "INFL_TOKEN")]
     infl_token: String,
+
+    #[arg(long, env = "LOG_LEVEL", default_value = "info")]
+    log_level: String,
 }
 
 #[tokio::main]
 async fn main() -> ThermostazvResult {
+    let args = Args::parse();
+
     let subscriber = tracing_subscriber::FmtSubscriber::builder()
-        .with_max_level(tracing::Level::TRACE)
+        .with_max_level(Level::from_str(&args.log_level)?)
         .finish();
     tracing::subscriber::set_global_default(subscriber)?;
-
-    let args = Args::parse();
 
     let thermostazv = Arc::new(RwLock::new(Thermostazv::new()));
     let status = Arc::new(RwLock::new(Cmd::Status(
@@ -70,7 +77,7 @@ async fn main() -> ThermostazvResult {
     let status_clone = Arc::clone(&status);
     let status_infl = Arc::clone(&status);
 
-    let mut uart_port = tokio_serial::new(args.uart_port, 2_000_000) // args.uard_baud)
+    let mut uart_port = tokio_serial::new(args.uart_port, args.uart_baud)
         .open_native_async()
         .context("Failed to open serial port")?;
     uart_port.set_exclusive(false)?;
@@ -85,7 +92,7 @@ async fn main() -> ThermostazvResult {
 
     let lwt = LastWill::new("/azv/thermostazv/lwt", "Offline", QoS::AtLeastOnce, false);
 
-    let mut mqttoptions = MqttOptions::new("thermostazv2", args.mqtt_host, 1883); //args.mqtt_port);
+    let mut mqttoptions = MqttOptions::new("thermostazv2", args.mqtt_host, args.mqtt_port);
     mqttoptions.set_keep_alive(Duration::from_secs(5));
     mqttoptions.set_last_will(lwt);
     mqttoptions.set_credentials(args.mqtt_user, args.mqtt_pass);
